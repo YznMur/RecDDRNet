e8f05f7899ea0fb90b151c3b1bf7e1bf038a5e75
YznMur <yazan.murhij@gmail.com>
2026-06-10 10:59:45 +0300

    signlegpu

--- scripts/train.sh
+++ scripts/train.sh
@@ -7,4 +7,4 @@
 # python tools/train.py --cfg experiments/rsm/ddrnet23_slim_640x480_5classes.yaml MODEL.NAME ddrnet23_slim_convlstm
 # python tools/eval.py --cfg experiments/rsm/ddrnet23_slim_640x480_5classes.yaml
 # python tools/demo.py --cfg configs/rsm_ddrnet23slim_convlstm.yaml
-python tools/train.py --cfg configs/rsm_ddrnet23slim_convlstm.yaml MODEL.KEYFRAME_INTERVAL 5
+python tools/train.py --cfg configs/rsm_ddrnet23slim_convlstm.yaml
