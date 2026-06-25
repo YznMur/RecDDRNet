eafc62536a21f7d737d464b9a41931e3709c3e28
YznMur <yazan.murhij@gmail.com>
2026-06-25 19:58:28 +0300

    multigpu_fixes

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,2 +1,3 @@
 
-python tools/train.py --cfg configs/rsm_ddrnet23slim_temporal_attn.yaml
+# python tools/train.py --cfg configs/rsm_ddrnet23slim_temporal_attn.yaml
+NCCL_DEBUG=INFO NCCL_SOCKET_IFNAME=^docker0,lo,br-* NCCL_IB_DISABLE=1 NCCL_P2P_DISABLE=1 NCCL_SHM_DISABLE=1 MASTER_PORT=29501 python3 -m torch.distributed.launch --nproc_per_node=2 --master_port=29501 tools/train.py --cfg configs/rsm_ddrnet23slim_temporal_attn.yaml
